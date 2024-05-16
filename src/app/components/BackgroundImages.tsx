0d10ee992c478e1220455317a0f989d5345c16a7
Ali Asif <ali.asifkhatry@gmail.com>
2024-05-16 16:15:50 +0500

    bgimgs
    Co-authored-by: hadi-14 <hadi-14@users.noreply.github.com>

--- src/app/components/BackgroundImages.tsx
+++ src/app/components/BackgroundImages.tsx
@@ -1,4 +1,5 @@
 // components/BackgroundImages.tsx
+"use client";
 
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
@@ -24,16 +25,16 @@ const BackgroundImages = () => {
 
   return (
     <div className={`fixed inset-0 z-0 overflow-hidden transition-opacity duration-1000 ${scrolled ? 'opacity-0' : 'opacity-100'}`}>
-      <div className="absolute w-full h-full">
+      <div className="absolute w-full h-1/2r">
         <Image src="/mountains/Layer1.png" alt="Image 1" layout="fill" objectFit="cover" />
       </div>
-      <div className="absolute w-full h-full">
+      <div className="absolute w-full h-1/2">
         <Image src="/mountains/Layer2.png" alt="Image 2" layout="fill" objectFit="cover" />
       </div>
-      <div className="absolute w-full h-full">
+      <div className="absolute w-full h-1/2">
         <Image src="/mountains/Layer3.png" alt="Image 3" layout="fill" objectFit="cover" />
       </div>
-      <div className="absolute w-full h-full">
+      <div className="absolute w-full h-1/2">
         <Image src="/mountains/Layer4.png" alt="Image 4" layout="fill" objectFit="cover" />
       </div>
     </div>
