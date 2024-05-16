b71d2808476c4a969a4c71dea9c93739aa915b79
Ali Asif <ali.asifkhatry@gmail.com>
2024-05-16 19:42:19 +0500

    bg

--- src/app/components/BackgroundImages.tsx
+++ src/app/components/BackgroundImages.tsx
@@ -25,16 +25,16 @@ const BackgroundImages = () => {
 
   return (
     <div className={`fixed inset-0 z-0 overflow-hidden transition-opacity duration-1000 ${scrolled ? 'opacity-0' : 'opacity-100'}`}>
-      <div className="absolute w-full h-1/2r">
+      <div className="absolute w-full h-full">
         <Image src="/mountains/Layer1.png" alt="Image 1" layout="fill" objectFit="cover" />
       </div>
-      <div className="absolute w-full h-1/2">
+      <div className="absolute w-full h-full">
         <Image src="/mountains/Layer2.png" alt="Image 2" layout="fill" objectFit="cover" />
       </div>
-      <div className="absolute w-full h-1/2">
+      <div className="absolute w-full h-full">
         <Image src="/mountains/Layer3.png" alt="Image 3" layout="fill" objectFit="cover" />
       </div>
-      <div className="absolute w-full h-1/2">
+      <div className="absolute w-full h-full">
         <Image src="/mountains/Layer4.png" alt="Image 4" layout="fill" objectFit="cover" />
       </div>
     </div>
