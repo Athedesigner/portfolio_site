b54b7f972fe0e801a9e97f5940bf2632434dadd9
Ali Asif <ali.asifkhatry@gmail.com>
2024-05-17 15:44:47 +0500

    bg-all done
    Co-authored-by: hadi-14 <hadi-14@users.noreply.github.com>

--- src/app/components/BackgroundImages.tsx
+++ src/app/components/BackgroundImages.tsx
@@ -1,19 +1,15 @@
 // components/BackgroundImages.tsx
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, CSSProperties } from 'react';
 import Image from 'next/image';
 
 const BackgroundImages = () => {
-  const [scrolled, setScrolled] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
+      setScrollPosition(window.scrollY);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -23,19 +19,40 @@ const BackgroundImages = () => {
     };
   }, []);
 
+  const getStyle = (endPosition: number, offset: number): CSSProperties & { [key: string]: string | number } => ({
+    '--bounce-up-end': `${endPosition}%`,
+    opacity: 1 - (scrollPosition - offset) / 200,
+    transform: `translateY(${Math.max(-1 * (1 - (scrollPosition - offset) / 2), endPosition)}%)`
+  });
+
   return (
-    <div className={`fixed inset-0 z-0 overflow-hidden transition-opacity duration-1000 ${scrolled ? 'opacity-0' : 'opacity-100'}`}>
-      <div className="absolute w-full h-full">
-        <Image src="/mountains/Layer1.png" alt="Image 1" layout="fill" objectFit="cover" />
-      </div>
-      <div className="absolute w-full h-full">
-        <Image src="/mountains/Layer2.png" alt="Image 2" layout="fill" objectFit="cover" />
-      </div>
-      <div className="absolute w-full h-full">
-        <Image src="/mountains/Layer3.png" alt="Image 3" layout="fill" objectFit="cover" />
-      </div>
-      <div className="absolute w-full h-full">
-        <Image src="/mountains/Layer4.png" alt="Image 4" layout="fill" objectFit="cover" />
+    <div className="fixed inset-0 z-0 overflow-hidden">
+      <div className="absolute w-full h-full bottom-0">
+        <div
+          className="absolute inset-0 translate-y-[35%] animate-bounce-up"
+          style={getStyle(35, 400)}
+        >
+          <Image src="/mountains/Layer1.png" alt="Image 1" layout="fill" objectFit="cover" />
+        </div>
+        <div
+          className="absolute inset-0 translate-y-[40%] animate-bounce-up"
+          style={getStyle(40, 300)}
+        >
+          <Image src="/mountains/Layer2.png" alt="Image 2" layout="fill" objectFit="cover" />
+        </div>
+        <div
+          className="absolute inset-0 translate-y-[45%] animate-bounce-up"
+          style={getStyle(45, 200)}
+        >
+          <Image src="/mountains/Layer3.png" alt="Image 3" layout="fill" objectFit="cover" />
+        </div>
+        <div
+          className="absolute inset-0 translate-y-[50%] animate-bounce-up"
+          style={getStyle(50, 100)}
+        >
+          <Image src="/mountains/Layer4.png" alt="Image 4" layout="fill" objectFit="cover" />
+        </div>
+        <div className="vignette"></div> {/* Vignette overlay */}
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,33 @@ import BackgroundImages from "./components/BackgroundImages";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
       <BackgroundImages />
-      <div className="relative z-20 p-8">
+      <div className="relative z-20 p-8 text-white opacity-0">
         <h1 className="text-4xl font-bold">Welcome to My Next.js App</h1>
         <p className="mt-4 text-lg">Scroll down to see the background images disappear.</p>
         {/* Add more content here */}
+        <br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>This one Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad commodi exercitationem. Recusandae, laborum! Quaerat nemo sint modi optio nostrum, nobis perferendis, rerum suscipit esse, aliquam reprehenderit dicta voluptate aperiam.</p><br />
       </div>
 
     </main>
