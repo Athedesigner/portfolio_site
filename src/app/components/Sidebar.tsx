00fa70648878faecf985659349f8a4e471ff46a0
Ali Asif <ali.asifkhatry@gmail.com>
2024-05-20 12:54:47 +0500

    hamburger icon corrected

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -9,16 +9,10 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
   return (
     <div
-      className={`fixed top-0 right-0 md:w-64 w-2/3 h-full h-fullblack bg-sidebar text-white transform ${
+      className={`fixed top-0 right-0 md:w-64 w-2/3 h-full bg-sidebar text-white transform ${
         isOpen ? 'translate-x-0' : 'translate-x-full'
       } transition-transform duration-300 ease-in-out z-30`}
     >
-      <button
-  className="absolute top-6 right-10 rounded-full text-6xl text-yellow-400" // Adjust the padding to increase button size
-  onClick={onClose}
->
-        &times;
-      </button>
       <nav className="mt-16">
         <ul className="space-y-4">
           <li><a href="#section1" className="block px-4 py-2">Section 1</a></li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useEffect, useState } from 'react';
-import Image from 'next/image';
 import BackgroundImages from './components/BackgroundImages';
 import Sidebar from './components/Sidebar';
 import './globals.css'; // Ensure this path points to your global stylesheet
@@ -47,10 +46,10 @@ export default function Home() {
     // Render loading animation while content is loading
     return (
       <div className="h-full min-h-screen flex items-center justify-center">
-      <div className="h-full w-full min-h-[500px] flex justify-center items-center">
-        <HelloAnimation />
+        <div className="h-full w-full min-h-[500px] flex justify-center items-center">
+          <HelloAnimation />
+        </div>
       </div>
-    </div>
     );
   }
 
@@ -58,14 +57,18 @@ export default function Home() {
     <main className={`flex min-h-screen flex-col items-center justify-between px-[15%] md:px-24 pt-24 bg-black`}>
       <BackgroundImages />
       <div className={`logo fixed top-7 left-10 z-20 ${bounceAnimation}`}>
-        <Image src="/logo.png" alt="Logo" width={80} height={45} />
+        <img src="/logo.png" alt="Logo" width={80} height={45} />
       </div>
-      <div
-        className={`hamburger fixed top-7 right-10 z-20 ${bounceAnimation}`}
+      <button
+        className={`hamburger fixed top-12 right-10 z-50 ${bounceAnimation}`}
         onClick={() => setSidebarOpen(!isSidebarOpen)}
       >
-        <Image src="/hamburger-icon.png" alt="Hamburger Menu" width={40} height={40} />
-      </div>
+        <div className={`hamburger-icon ${isSidebarOpen ? 'active' : ''}`}>
+          <span className="line" />
+          <span className="line" />
+          <span className="line" />
+        </div>
+      </button>
       <Sidebar isOpen={isSidebarOpen} onClose={() => setSidebarOpen(false)} />
       <div className="relative z-10 text-white">
         <h1 className="text-4xl font-bold">Welcome to My Next.js App</h1>
